a94a1e1c30a01c76caa1e4ad505b8888f0f706ab
Jon Law <545310+jonnie1987@users.noreply.github.com>
2020-12-13 17:03:37 +0000

    Add ability to modify player movement speed

--- control.lua
+++ control.lua
@@ -1,3 +1,7 @@
+local function pick_random_player ()
+    return game.players[math.random(#game.players)]
+end
+
 remote.add_interface("twitch_deathworld",{
     help_research = function (name, amount)
         local player = game.players[1];
@@ -35,5 +39,29 @@ remote.add_interface("twitch_deathworld",{
             return "worked";
         end
         return "failed";
+    end,
+
+    modify_run_speed = function (name, target_player_name, modifier)
+        if target_player_name == "random_player" then
+            target_player_name = pick_random_player().name
+        end
+        
+        modifier = tonumber(modifier)
+
+        for i,player in pairs(game.players) do
+            if target_player_name == "all_players" or target_player_name == player.name then
+                if player.character ~= nil then
+                    local base_speed = player.character["character_running_speed_modifier"]
+                    local resultant_speed = base_speed + modifier
+                    player.character["character_running_speed_modifier"] = resultant_speed
+                    if modifier > 0 then
+                        player.force.print({"", "[Twitch] ", name, " Buffed ", player.name, "'s run speed"}, {0, 1, 0, 1});
+                    else
+                        player.force.print({"", "[Twitch] ", name, " Nerfed ", player.name, "'s run speed"}, {1, 0, 0, 1});
+                    end
+                end
+            end
+        end
     end
 });
+
