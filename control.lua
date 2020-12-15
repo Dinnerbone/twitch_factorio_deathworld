323e2c8fe376d396200017d3c20290df834e54ce
Jon Law <545310+jonnie1987@users.noreply.github.com>
2020-12-15 15:58:27 +0000

    switch player targeting to be common and singular add limits to speed modifier

--- control.lua
+++ control.lua
@@ -1,5 +1,18 @@
-local function pick_random_player ()
-    return game.players[math.random(#game.players)]
+local function get_target_player_from_name (target_name)
+    if target_name == "*" or target_name == "" then
+        return game.players[math.random(#game.players)]
+    end
+
+    local target_player = game.get_player(target_name);
+    if target_player == nil then
+        for _, player in pairs(game.players) do
+            if string.find(string.lower(player.name), string.lower(target_name)) then
+                target_player = player; 
+            end
+        end
+    end
+
+    return target_player
 end
 
 remote.add_interface("twitch_deathworld",{
@@ -47,21 +60,7 @@ remote.add_interface("twitch_deathworld",{
       
     plant_tree = function (targetName, amount)
         local planted = false;
-
-        local targetPlayer = game.get_player(targetName);
-
-        if targetName == "*" or targetName == "" then
-            local playerCount = #game.players;
-            targetPlayer = game.players[math.random(1, playerCount)];
-        else 
-            if targetPlayer == nil then
-                for _, player in pairs(game.players) do
-                    if string.find(string.lower(player.name), string.lower(targetName)) then
-                        targetPlayer = player; 
-                    end
-                end
-            end
-        end
+        local targetPlayer = get_target_player_from_name(targetName)
 
         if targetPlayer then
             for i = 1, tonumber(amount) do
@@ -85,32 +84,44 @@ remote.add_interface("twitch_deathworld",{
         return;
     end,
 
-    modify_run_speed = function (name, target_player_name, modifier)
-        if target_player_name == "random_player" then
-            target_player_name = pick_random_player().name
-        end
-        
-        modifier = tonumber(modifier)
-        target_player_name = target_player_name.lower
-        for _,player in pairs(game.players) do
-            if target_player_name == "all_players" or target_player_name == player.name.lower then
-                if player.character ~= nil then
-                    local base_speed = player.character["character_running_speed_modifier"]
-                    local resultant_speed = base_speed + modifier
-                    player.character["character_running_speed_modifier"] = resultant_speed
-                    if modifier > 0 then
-                        player.force.print({"", "[Twitch] ", name, " Buffed ", player.name, "'s run speed"}, {0, 1, 0, 1});
-                    else
-                        player.force.print({"", "[Twitch] ", name, " Nerfed ", player.name, "'s run speed"}, {1, 0, 0, 1});
-                    end
-                else
-                    rcon.print({"failed|Unable to find player character for ", player.name});
-                    return;
+    modify_run_speed = function (name, target_player_name, modifier_change, lower_limit, upper_limit)
+        local target_player = get_target_player_from_name(target_player_name)       
+        lower_limit = lower_limit or 0.5
+        upper_limit = upper_limit or 3
+        modifier_change = tonumber(modifier_change)
+
+        if target_player and target_player.character ~= nil then
+            local current_speed_modifier = target_player.character["character_running_speed_modifier"]
+            local resultant_speed_modifier = current_speed_modifier + modifier_change
+            
+            local limits_reached = false
+            if resultant_speed_modifier < lower_limit then
+                resultant_speed_modifier = lower_limit
+                if current_speed_modifier == lower_limit then
+                    limits_reached = true
                 end
-            else
-                rcon.print("failed|Unable to find player by that name");
-                return;
+            elseif resultant_speed_modifier > 3 then
+                resultant_speed_modifier = 3
+                if current_speed_modifier == upper_limit then
+                    limits_reached = true
+                end
             end
+
+            if limits_reached then
+                rcon.print("failed|Player at speed threshold already");
+                return;
+            else
+                target_player.character["character_running_speed_modifier"] = resultant_speed_modifier
+
+                if modifier_change > 0 then
+                    target_player.force.print({"", "[Twitch] ", name, " Buffed ", target_player.name, "'s run speed"}, {0, 1, 0, 1});
+                else
+                    target_player.force.print({"", "[Twitch] ", name, " Nerfed ", target_player.name, "'s run speed"}, {1, 0, 0, 1});
+                end
+            end
+        else
+            rcon.print("failed|Unable to find player by that name");
+            return;
         end
         rcon.print("worked");
         return;
